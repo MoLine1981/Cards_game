53c41315f9c2d92b60a0f26df118cf5aad3b5d08
Monika <zickutem@gmail.com>
2016-11-30 17:22:20 +0200

    Initial commit

---
+++ Cards game - Android/cupsn/app/src/main/java/com/example/monika/cupsn/MainActivity.java
@@ -0,0 +1,152 @@
+package com.example.monika.cupsn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView right, middle, left;
+
+    Button new_game;
+
+    List<Integer> cards;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        left = (ImageView) findViewById(R.id.left);
+        middle = (ImageView) findViewById(R.id.middle);
+        right = (ImageView) findViewById(R.id.right);
+
+        new_game = (Button) findViewById(R.id.new_game);
+
+        cards = new ArrayList<>();
+        cards.add(107);
+        cards.add(207);
+        cards.add(407);
+
+        Collections.shuffle(cards);
+
+        new_game.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+
+            public void onClick(View view){
+                Collections.shuffle(cards);
+
+                left.setImageResource(R.drawable.ic_cardback);
+                middle.setImageResource(R.drawable.ic_cardback);
+                right.setImageResource(R.drawable.ic_cardback);
+
+                Animation anim_left = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.left);
+                Animation anim_middle = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.middle);
+                Animation anim_right = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.right);
+
+                left.startAnimation(anim_left);
+                middle.startAnimation(anim_middle);
+                right.startAnimation(anim_right);
+
+
+            }
+        });
+
+
+        left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    if(cards.get(0) == 107){
+                        Toast.makeText(MainActivity.this, "Indeed!", Toast.LENGTH_SHORT).show();
+                        left.setImageResource(R.drawable.ic_spades);
+                    }   else if(cards.get(0) == 207){
+                        left.setImageResource(R.drawable.ic_hearts);
+                    }   else if(cards.get(0) == 407){
+                        left.setImageResource(R.drawable.ic_diamonds);
+                    }
+                    if(cards.get(1) == 107){
+                        middle.setImageResource(R.drawable.ic_spades);
+                    }   else if(cards.get(1) == 207){
+                        middle.setImageResource(R.drawable.ic_hearts);
+                    }   else if(cards.get(1) == 407){
+                        middle.setImageResource(R.drawable.ic_diamonds);
+                    }
+
+                    if(cards.get(2) == 107){
+                        right.setImageResource(R.drawable.ic_spades);
+                    }   else if(cards.get(2) == 207){
+                        right.setImageResource(R.drawable.ic_hearts);
+                    }   else if(cards.get(2) == 407){
+                        right.setImageResource(R.drawable.ic_diamonds);
+                    }
+            }
+        });
+        middle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(cards.get(1) == 107){
+                    Toast.makeText(MainActivity.this, "Indeed!", Toast.LENGTH_SHORT).show();
+                    middle.setImageResource(R.drawable.ic_spades);
+                }   else if(cards.get(1) == 207){
+                    middle.setImageResource(R.drawable.ic_hearts);
+                }   else if(cards.get(1) == 407){
+                    middle.setImageResource(R.drawable.ic_diamonds);
+                }
+                if(cards.get(0) == 107){
+                    left.setImageResource(R.drawable.ic_spades);
+                }   else if(cards.get(0) == 207){
+                    left.setImageResource(R.drawable.ic_hearts);
+                }   else if(cards.get(0) == 407){
+                    left.setImageResource(R.drawable.ic_diamonds);
+                }
+                if(cards.get(2) == 107){
+                    right.setImageResource(R.drawable.ic_spades);
+                }   else if(cards.get(2) == 207){
+                    right.setImageResource(R.drawable.ic_hearts);
+                }   else if(cards.get(2) == 407){
+                    right.setImageResource(R.drawable.ic_diamonds);
+                }
+
+            }
+        });
+        right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(cards.get(2) == 107){
+                    right.setImageResource(R.drawable.ic_spades);
+                }   else if(cards.get(2) == 207){
+                    right.setImageResource(R.drawable.ic_hearts);
+                }   else if(cards.get(2) == 407){
+                    right.setImageResource(R.drawable.ic_diamonds);
+                }
+                if(cards.get(1) == 107){
+                    right.setImageResource(R.drawable.ic_spades);
+                }   else if(cards.get(1) == 207){
+                    right.setImageResource(R.drawable.ic_hearts);
+                }   else if(cards.get(1) == 407){
+                    right.setImageResource(R.drawable.ic_diamonds);
+                }
+                if(cards.get(0) == 107){
+                    right.setImageResource(R.drawable.ic_spades);
+                    Toast.makeText(MainActivity.this, "Indeed!", Toast.LENGTH_SHORT).show();
+                }   else if(cards.get(0) == 207){
+                    right.setImageResource(R.drawable.ic_hearts);
+                }   else if(cards.get(0) == 407){
+                    right.setImageResource(R.drawable.ic_diamonds);
+                }
+
+
+            }
+        });
+    }
+    }
